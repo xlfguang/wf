56c0c394cd211d85b98abe181aff1c8f79046ff9
guang <d405107912@gmail.com>
2025-01-10 00:48:09 +0800

    yarn

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from '@/pages/home/index';
-
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "@/pages/home/index";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={
-         <Home/>
-        } />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
