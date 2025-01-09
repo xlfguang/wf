ed650a1f06e148fe5d67ee78f7e51f5a421f04ef
guang <d405107912@gmail.com>
2025-01-10 00:54:12 +0800

    11

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./wf",
+  base: "/wf",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
