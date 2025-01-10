3a908979b1b2814f09c30e53d3c734642b11db29
guang <d405107912@gmail.com>
2025-01-11 00:34:10 +0800

    feat:注册

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,17 @@
 import { LeftImg, RightImg } from "../home/style";
 import {
+  Line,
   LoginBody,
   LoginBox,
+  LoginBtn,
   LoginForm,
   LoginFormTitle,
   LoginInput,
   LoginLogo,
+  SwtichBtn,
+  Tips,
+  TipsTow,
+  TipsTowBox,
 } from "./style";
 import leftImg from "@/assets/left.png";
 import rightImg from "@/assets/right.png";
@@ -23,6 +29,16 @@ function index() {
           <LoginInput placeholder="Password" />
           <LoginInput placeholder="Confirm Password" />
           <LoginInput placeholder="Invite Code" />
+          <LoginBtn>Register</LoginBtn>
+          <Tips>
+            By clicking on register, you agree to Tunnel's Terms ofService and
+            Privacy Policy
+          </Tips>
+          <Line />
+          <TipsTowBox>
+            <TipsTow>Already have an account？</TipsTow>
+            <SwtichBtn>Login</SwtichBtn>
+          </TipsTowBox>
         </LoginForm>
       </LoginBox>
     </LoginBody>
--- src/pages/login/style.tsx
+++ src/pages/login/style.tsx
@@ -52,3 +52,60 @@ export const LoginInput = styled.input`
   border-radius: 8px;
   margin-bottom: 12px;
 `;
+
+export const LoginBtn = styled.button`
+  background: linear-gradient(
+    92.39deg,
+    #ffffff 5.83%,
+    #ffffff 15.48%,
+    #fcf605 66.12%,
+    #45f836 110.21%
+  );
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 24px;
+  padding: 16px 0;
+  width: 100%;
+  border-radius: 8px;
+  margin-top: 65px;
+`;
+
+export const Tips = styled.div`
+  width: 342px;
+  height: 36px;
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 18px;
+  text-align: center;
+  text-underline-position: from-font;
+  text-decoration-skip-ink: none;
+  color: #ffffff80;
+  margin: 16px auto;
+`;
+export const Line = styled.div`
+  width: 100%;
+  height: 1px;
+  border: 0.5px solid #90a6c566;
+  margin-bottom: 24px;
+`;
+
+export const TipsTowBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  margin: 35px auto 0;
+  justify-content: center;
+`;
+export const TipsTow = styled.div`
+  font-size: 12px;
+  color: #ffffff80;
+  font-weight: 400;
+`;
+export const SwtichBtn = styled.button`
+  border: 1px solid #ffffff14;
+  color: #ffffff;
+  font-size: 14px;
+  line-height: 21px;
+  padding: 10px 26px;
+  border-radius: 4px;
+`;
